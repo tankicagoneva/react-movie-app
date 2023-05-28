3f248971acbbee5cd9f3e2a2a9c306be4ff6bc48
tankicagoneva <tankicaspasev@gmail.com>
2023-05-28 19:08:35 +0100

    feat: introduce heroImage component

--- react-rmdb/src/components/Home.js
+++ react-rmdb/src/components/Home.js
@@ -9,7 +9,7 @@ import {
   API_URL,
 } from "../config";
 
-import Header from "./elements/Header";
+// import Header from "./elements/Header";
 import HeroImage from "./elements/HeroImage";
 import SearchBar from "./elements/SearchBar";
 import Grid from "./elements/Grid";
@@ -24,10 +24,17 @@ import { useHomeFetch } from "./hooks/useHomeFetch";
 const Home = () => {
   const [{ state, loading, error }, fetchMovies] = useHomeFetch();
   console.log(state);
+
+  if (error) return <div>Something went wrong...</div>;
+  if (!state.movies[0]) return <Spinner />;
+
   return (
     <>
-      <Header />
-      <HeroImage />
+      <HeroImage
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
+        title={state.heroImage.original_title}
+        text={state.heroImage.overview}
+      />
       <SearchBar />
       <Grid />
       <MovieThumb />
